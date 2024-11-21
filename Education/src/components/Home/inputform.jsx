cd6f0a678d65df75a1691c4248e65803538eaa17
vishnupatel45 <patelvishnu4518@gmail.com>
2024-11-21 17:24:49 +0530

    feat:updated login page

--- Education/src/components/Home/inputform.jsx
+++ Education/src/components/Home/inputform.jsx
@@ -4,38 +4,35 @@ import './inputform.css'
 const Inputform = () => {
   return (
     <>
-       <div className='form-con'>
-                      <form action="https://formspree.io/f/mdoqbvzw" method="POST"role="form" >
-                       
-                        <div class="row">
-                        <h1>Please enter your details</h1>
-                          <div class="col-md-12 mb-3">
-                            <div class="form-group">
-                              <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required/>
-                            </div>
-                          </div>
-                          <div class="col-md-12 mb-3">
-                            <div class="form-group">
-                              <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required/>
-                            </div>
-                          </div>
-                          <div class="col-md-12 mb-3">
-                            <div class="form-group">
-                              <input type="text" class="form-control" name="mobileno" id="mobileno" placeholder="Mobileno" required/>
-                            </div>
-                          </div>
-                         
-                          {/* <div class="col-md-12 text-center my-3">
-                            <div class="loading">Loading</div>
-                            <div class="error-message"></div>
-                            <div class="sent-message">Your message has been sent. Thank you!</div>
-                          </div> */}
-                          <div class="col-md-12 text-center">
-                            <button type="submit" class="button button-a button-big button-rouded">Send Message</button>
-                          </div>
-                        </div>
-                      </form>
-                    </div>
+      <div className='form-con d-flex justify-content-center align-items-center' style={{height:'80vh'}}>
+        <div className=' logincontainer p-3 rounded-3 border-1 shadow bg-dark-subtle'>
+          <form action="https://formspree.io/f/mdoqbvzw" method="POST" role="form" >
+
+            <div class="row">
+              <div className='fw-medium fs-3' >Please enter your details</div>
+              <hr />
+              <div className="col-md-12 mb-3">
+                <div className="form-group">
+                  <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
+                </div>
+              </div>
+              <div className="col-md-12 mb-3">
+                <div className="form-group">
+                  <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
+                </div>
+              </div>
+              <div className="col-md-12 mb-3">
+                <div className="form-group">
+                  <input type="text" className="form-control" name="mobileno" id="mobileno" placeholder="Mobileno" required />
+                </div>
+              </div>
+              <div className="col-md-12 text-center">
+                <button type="submit" className="btn btn-info">Send Message</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
